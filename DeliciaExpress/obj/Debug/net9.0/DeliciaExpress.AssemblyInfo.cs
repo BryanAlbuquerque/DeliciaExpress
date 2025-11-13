c8ba3e2d920130f2a3b6de4504673bfabdc38cc3
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-11-13 12:27:12 -0300

    Testes banco de dados

--- DeliciaExpress/obj/Debug/net9.0/DeliciaExpress.AssemblyInfo.cs
+++ DeliciaExpress/obj/Debug/net9.0/DeliciaExpress.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliciaExpress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4581c21b5eef11ea67b7d1d15ced231091825856")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42127c9435c968681c6cd9b732798015662bd49")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliciaExpress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliciaExpress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
